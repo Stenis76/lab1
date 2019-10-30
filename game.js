bfb01d97546549e3ac8d22aef5e891ee9f5a3b9d
Dick Stenqvist <stenqvist.dick@gmail.com>
2019-10-30 09:30:55 +0100

    stommen är satt, använder mig av youtubetutorial

---
+++ game.js
@@ -0,0 +1,15 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement  = document.getElementById('option-buttons')
+
+// startar spelet, sätter igång applicationen där den ska börja
+function startGame() {
+
+}
+// Visar vilken val vi är på
+function showTextNode(textNodeIndex) {
+
+}
+//En funktion som körs varje gång vi ska gör ett val
+function selectOption(option) {
+
+}
