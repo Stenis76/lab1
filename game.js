a49ceb0d764721881decf5c741c45068ebd35458
Dick Stenqvist <stenqvist.dick@gmail.com>
2019-11-01 10:02:51 +0100

    Lagt till fullt spelflöde

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
 const textElement = document.getElementById('text')
 const optionListItems = document.getElementById('optionlist')
 
-//håller koll på vad karaktären har gjort och har med sig
-let state = {}
 let textNode;
+
+//håller koll på vad karaktären har gjort och har med sig, då jag ännu inte använder mig av 
+//states än så fyller den ingen funktion
+let state = {}
 
 
 /**
@@ -26,20 +28,28 @@ function showTextNode(textNodeIndex) {
     while (optionListItems.firstChild) {
         optionListItems.removeChild(optionListItems.firstChild)
     }
-    // sätta ut alterntiv för våra val i form av en lista
-
+    
+/**
+ * Sätta ut alterntiv för våra val i form av en lista
+ * 
+ */
     textNode.options.forEach(option => {
-        // if (showOption(option)) {
         const listItem = document.createElement('li')
         listItem.innerText = option.text
         optionListItems.appendChild(listItem)
+        // Denna kod behövs för att fixa olika states, men gör detta i vers2
+        // if (showOption(option)) {
         // list.addEventListener('click', () => selectOption(option))
         // optionButtonsElement.appendChild(button)
         // }
     })
 }
 
-//En funktion som körs varje gång vi ska gör ett val
+
+/**
+ * En funktion som körs varje gång vi ska gör ett val (frågan e om)
+ * @param {string} input 
+ */
 function selectOption(input) {
     const option = textNode.options.find(option => option.text.toLowerCase() === input.toLowerCase())
 
@@ -50,7 +60,7 @@ function selectOption(input) {
 
     const nextTextNodeId = option.nextText
     if (nextTextNodeId <= 0) return startGame()
-
+    // Använder mig av state i version 2
     // state = Object.assign(state, option.setState)
     showTextNode(nextTextNodeId)
 }
@@ -70,15 +80,15 @@ function inputValue() {
 const textNodes = [
     {
         id: 1,
-        text: 'After a real good night out with the lads you wake up in your bed next to your sleeping wife. Your head is killing you, and yore mouth is dry as a desert. There´s some asperin in the bedside drawer',
+        text: 'After a real good night out with the lads you wake up in your bed next to your sleeping wife. Your head is killing you, and yore mouth is dry as a desert. There´s some pills in the bedside drawer',
         options: [
             {
                 text: 'Take pills',
-                setState: { pills: true },
-                nextText: 2,
+               // setState: { pills: true },
+                nextText: 6,
             },
             {
-                text: 'leave them',
+                text: 'Leave them',
                 nextText: 2,
             }
         ]
@@ -87,19 +97,24 @@ const textNodes = [
         id: 2,
         text: 'You silently try to get out of bed without waking the beast sleeping next to you. You go out to the kitchen to get yourself a glass of water',
         options: [
+            // {
+            //     text: 'Drink water and use pills',
+            //     requiredState: (currentState) => currentState.pills,
+            //     setState: { pills: false, noHeadache: true, noThirst: true },
+            //     nextText: 3,
+            // },
             {
-                text: 'Drink water and use pills',
-                requiredState: (currentState) => currentState.pills,
-                setState: { pills: false, noHeadache: true, noThirst: true },
+                text: 'Drink water from tap',
+                // setState: { noThirst: true },
+                nextText: 7,
+            },
+            {
+                text: 'Pour a glas of water',
+                // setState: { noThirst: true },
                 nextText: 3,
             },
             {
-                text: 'Drink water',
-                setState: { noThirst: true },
-                nextText: 3,
-            },
-            {
-                text: 'Go to bed',
+                text: 'Go back to bed',
                 nextText: 4,
             }
         ]
@@ -114,7 +129,7 @@ const textNodes = [
             },
             {
                 text: 'hide in bathroom',
-                nextText: 6,
+                nextText: 8,
             }
         ]
 
@@ -131,19 +146,117 @@ const textNodes = [
     },
     {
         id: 5,
-        text: 'The world around you have started to stablize a bit, suddenly one of your kids starts to cry from the bedroom. What do you do?',
+        text: 'The baby screaming and you know if he wakes upp the house youre going to be the one too suffer, What do you do?',
         options: [
+            // {
+            //     text: 'With no headache, u easily comfort the baby',
+            //     requiredState: (currentState) => currentState.noHeadache,
+            //     nextText: 7,
+            // },
             {
-                text: 'With no headache, u easily comfort the baby',
-                requiredState: (currentState) => currentState.noHeadache,
-                nextText: 7,
+                text: 'U try to make the baby silent with a song',
+                nextText: 9,
             },
             {
-                text: 'U try to make the baby quite',
-                nextText: 8,
+                text: 'U try to make the baby silent with a pillow',
+                nextText: 10,
             }
         ]
 
+    },
+    {
+        id: 6,
+        text: 'You swallow the pills without checking the label, why on earth would someone store cyanidpills in there bedside drawer.. Youre dead!',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1,
+            }
+        ]
+    },
+    {
+        id: 7,
+        text: 'You drink the water from the tap, but its hot instead of cold.. you burn your mouth and scream and you wake the whole family.. Now u wish you were dead!',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1,
+            }
+        ]
+    },
+    {
+        id: 8,
+        text: 'You hide in the bathroom and you hear the heavy steps of youre spouse going to the nursery, you know that this wont end well so u take your life - Youre dead',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1,
+            }
+        ]
+    },
+    {
+        id: 9,
+        text:'You sing a lullaby and the baby falls asleep, what do u do now? You still got one hell of a headache and you should probably get some more water in your system',
+        options: [
+            {
+                text: 'Make breakfast',
+                nextText: 11,
+            },
+            {
+                text: 'Get pills from bedroom',
+                nextText: 6,
+            },
+            {
+                text: 'Fuck it - go to bed',
+                nextText: 4,
+            }
+
+        ]
+    },
+    {
+        id: 10,
+        text: 'You sick fuck, thats murder.. no more playing for u!!',
+        options: [
+            //kanske lägga in en infinte loop så webläsaren låser sig XD
+            {
+                text: 'Restart',
+                nextText: -1,
+            }
+        ]
+    },
+    {
+        id: 11,
+        text:'You make a real good breakfast, with egg and bacon, black coffe and some orange juice.',
+        options: [
+            {
+                text: 'Give it wife in bed',
+                nextText: 12,
+            },
+            {
+                text: 'Eat it',
+                nextText: 13,
+            }
+        ]
+    },
+    {
+        id: 12,
+        text:'You know what they say - happy wife, happy life! Congrats you survived',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1,
+            }
+        ]
+    },
+    {
+        id: 13,
+        text:'You eat the breakfast and you feel that youre life is comming back to u.. But then it happens youre wife wakes up hungry and theres no food left.. Youre dead!',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1,
+            }
+        ]
     },
 ]
 
